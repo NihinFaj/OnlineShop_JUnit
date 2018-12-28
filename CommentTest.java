c75293d129661f99d01c329e5902ee68fd1dda89
Nihinlolamiwa Fajemilehin <nihinfaj@gmail.com>
2018-12-28 14:55:51 +0000

    Upvote and Downvote test cases added

--- CommentTest.java
+++ CommentTest.java
@@ -37,4 +37,46 @@ public class CommentTest
     public void tearDown()
     {
     }
+
+    /**
+     * Test to see if author and rating are stored correctly when creating a comment
+     */
+    @Test
+    public void testCreateComment()
+    {
+        Comment comment1 = new Comment("John", "It works as advertised", 4);
+        assertEquals("John", comment1.getAuthor());
+        assertEquals(4, comment1.getRating());
+    }
+
+    /**
+     * Test to see if comment upvote works correctly
+     */
+    @Test
+    public void testUpvote()
+    {
+        Comment comment1 = new Comment("Dupe", "Theis makeup is great", 5);
+        comment1.upvote();
+        comment1.upvote();
+        comment1.upvote();
+        assertEquals(3, comment1.getVoteCount());
+    }
+
+    /**
+     * Test to see if comment downvote works correctly
+     */
+    @Test
+    public void testDownVote()
+    {
+        Comment comment1 = new Comment("Sam", "I wich I got the newer model, this one sucks!", 2);
+        comment1.upvote();
+        comment1.upvote();
+        comment1.upvote();
+        comment1.downvote();
+        comment1.downvote();
+        assertEquals(1, comment1.getVoteCount());
+    }
 }
+
+
+
